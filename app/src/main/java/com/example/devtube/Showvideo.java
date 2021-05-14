f4bd7819d181d0313908a0f2358420c33bbda117
OmkarGeedh <40747153+OmkarGeedh@users.noreply.github.com>
2021-05-14 19:51:39 +0530

    PUSh

---
+++ app/src/main/java/com/example/devtube/Showvideo.java
@@ -0,0 +1,66 @@
+//package com.example.devtube;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import com.firebase.ui.database.FirebaseRecyclerAdapter;
+//import com.firebase.ui.database.FirebaseRecyclerOptions;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//public class Showvideo extends AppCompatActivity {
+//
+//    DatabaseReference databaseReference;
+//    RecyclerView recyclerView;
+//    FirebaseDatabase database;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_showvideo);
+//
+//        recyclerView = findViewById(R.id.recyclerview_ShowVideo);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager( this));
+//        database = FirebaseDatabase.getInstance();
+//        databaseReference = database.getReference("video");
+//
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        FirebaseRecyclerOptions<Member> options =
+//                new FirebaseRecyclerOptions.Builder<Member>()
+//                .setQuery(databaseReference, Member.class)
+//                .build();
+//        FirebaseRecyclerAdapter<Member,ViewHolder> firebaseRecyclerAdapter =
+//                new FirebaseRecyclerAdapter<Member, ViewHolder>(options) {
+//                    @Override
+//                    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Member model) {
+//
+//                        holder.setExoplayer(getApplication(),model.getName(), model.getVideourl());
+//
+//                    }
+//
+//                    @NonNull
+//                    @Override
+//                    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//                        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent, false);
+//
+//                        return new ViewHolder(view);
+//                    }
+//                };
+//            firebaseRecyclerAdapter.startListening();
+//        }
+//
+//    }
--- app/src/main/java/com/example/devtube/ViewHolder.java
+++ app/src/main/java/com/example/devtube/ViewHolder.java
@@ -29,6 +29,8 @@ public class ViewHolder extends RecyclerView.ViewHolder {
     PlayerView playerView;
       private BandwidthMeter bandwidthMeter;
       private TrackSelector trackSelector;
+
+
       private TrackSelection.Factory trackSelectionFactory;
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
