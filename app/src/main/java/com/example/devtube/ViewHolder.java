a8d2e495dbf4d1ff85b83b1628fb2d8a3c0c779b
OmkarGeedh <40747153+OmkarGeedh@users.noreply.github.com>
2021-05-18 09:17:29 +0530

    Push

--- app/src/main/java/com/example/devtube/ViewHolder.java
+++ app/src/main/java/com/example/devtube/ViewHolder.java
@@ -1,46 +1,45 @@
-//package com.example.devtube;
-//
-//import android.app.Application;
-//import android.net.Uri;
-//import android.util.Log;
-//import android.view.View;
-//import android.widget.TextView;
-//
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.google.android.exoplayer2.ExoPlayerFactory;
-//import com.google.android.exoplayer2.SimpleExoPlayer;
-//import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-//import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-//import com.google.android.exoplayer2.source.ExtractorMediaSource;
-//import com.google.android.exoplayer2.source.MediaSource;
-//import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-//import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-//import com.google.android.exoplayer2.trackselection.TrackSelector;
-//import com.google.android.exoplayer2.ui.PlayerView;
-//import com.google.android.exoplayer2.upstream.BandwidthMeter;
-//import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-//import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-//
-//public class ViewHolder extends RecyclerView.ViewHolder {
-//
-//    SimpleExoPlayer exoPlayer;
-//    PlayerView playerView;
-//      private BandwidthMeter bandwidthMeter;
-//      private TrackSelector trackSelector;
-//      private TrackSelection.Factory trackSelectionFactory;
-//    public ViewHolder(@NonNull View itemView) {
-//        super(itemView);
-//    }
-//
-//    public void setExoplayer(Application application, String name, String Videourl){
-//
-//        TextView textView = itemView.findViewById(R.id.tv_item_name);
-//        playerView = itemView.findViewById(R.id.exoplayer_item);
-//        textView.setText(name);
-//
-//        try {
+package com.example.devtube;
+
+import android.app.Application;
+import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    SimpleExoPlayer exoPlayer;
+    PlayerView playerView;
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void setExoplayer(Application application, String name, String Videourl){
+
+        TextView textView = itemView.findViewById(R.id.tv_item_name);
+        playerView = itemView.findViewById(R.id.exoplayer_item);
+        textView.setText(name);
+
+        try {
 //            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter.Builder(application).build();
 //            TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
 //            exoPlayer = (SimpleExoPlayer) ExoPlayerFactory.newSimpleInstance(application);
@@ -51,10 +50,21 @@
 //            playerView.setPlayer(exoPlayer);
 //            exoPlayer.prepare(mediaSource);
 //            exoPlayer.setPlayWhenReady(false);
-//        }catch(Exception e){
-//            Log.e("ViewHolder", "exoplayer Error"+e.toString());
-//        }
-//    }
-//
-//}
-//
+
+            Uri uri = Uri.parse(Videourl);
+            SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(application);
+            DefaultHttpDataSourceFactory dataSourceFactory= new DefaultHttpDataSourceFactory("video");
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+            MediaSource mediaSource = new ExtractorMediaSource(uri, dataSourceFactory, extractorsFactory, null, null);
+            playerView.setPlayer(player);
+            player.prepare(mediaSource);
+            player.setPlayWhenReady(false);
+
+
+        }catch(Exception e){
+            Log.e("ViewHolder", "exoplayer Error"+e.toString());
+        }
+    }
+
+}
+
